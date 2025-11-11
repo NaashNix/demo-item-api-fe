01f6fbebf1f97a00e0a02b92c749bde8a114dd25
NaashNix <thilakshanaravindu@gmail.com>
2025-11-11 22:37:49 +0530

    PrimeReactProvider Installed and Applied

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,33 @@ import React from "react";
 import Menu from "./components/Menu";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ViewItem from "./components/viewItem/ViewItem";
-import AddItem from "./components/addItem/AddItem"
+import AddItem from "./components/addItem/AddItem";
 import DeleteItem from "./components/deleteItem/DeleteItem";
 import UpdateItem from "./components/updateItem/UpdateItem";
+import { PrimeReactProvider } from 'primereact/api';
+// import 'primeflex/primeflex.css';
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Menu></Menu>
+        <PrimeReactProvider>
+          <Menu></Menu>
 
-        <Routes>
-          <Route path="/" element={<ViewItem />} />
-          <Route path="/add" element={<AddItem />} />
-          <Route path="/update" element={<UpdateItem />} />
-          <Route path="/delete" element={<DeleteItem />} />
-        </Routes>
-      </BrowserRouter>
-      
-    </>
-  );
+          <Routes>
+            <Route path="/" element={<ViewItem />} />
+            <Route path="/add" element={<AddItem />} />
+            <Route path="/update" element={<UpdateItem />} />
+            <Route path="/delete" element={<DeleteItem />} />
+          </Routes>
+          </PrimeReactProvider>
+          </BrowserRouter>
+
+        </>
+        );
 }
 
-export default App;
+        export default App;
--- src/components/viewItem/ViewItem.jsx
+++ src/components/viewItem/ViewItem.jsx
@@ -1,45 +1,137 @@
-import React from 'react'
-import styles from './ViewItem.module.css'
+import React, { useState } from 'react';
+import styles from './ViewItem.module.css';
 import Header from '../utils/Header';
-import Table from 'react-bootstrap/Table';
+import Table from 'react-bootstrap/Table'; 
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
 
 function ViewItem() {
+
+  // const [products, setProducts] = useState();
+
+  const products = [{
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+    {
+      id: '1001',
+      code: 'nvklal433',
+      name: 'Black Watch',
+      description: 'Product Description',
+      image: 'black-watch.jpg',
+      price: 72,
+      category: 'Accessories',
+      quantity: 61,
+      inventoryStatus: 'INSTOCK',
+      rating: 4
+    },
+    {
+      id: '1002',
+      code: 'zz21cz3c1',
+      name: 'Blue Band',
+      description: 'Product Description',
+      image: 'blue-band.jpg',
+      price: 79,
+      category: 'Fitness',
+      quantity: 2,
+      inventoryStatus: 'LOWSTOCK',
+      rating: 3
+    },
+    {
+      id: '1003',
+      code: '244wgerg2',
+      name: 'Blue T-Shirt',
+      description: 'Product Description',
+      image: 'blue-t-shirt.jpg',
+      price: 29,
+      category: 'Clothing',
+      quantity: 25,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+    },
+    {
+      id: '1004',
+      code: 'h456wer53',
+      name: 'Bracelet',
+      description: 'Product Description',
+      image: 'bracelet.jpg',
+      price: 15,
+      category: 'Accessories',
+      quantity: 73,
+      inventoryStatus: 'INSTOCK',
+      rating: 4
+    },
+    {
+      id: '1005',
+      code: 'av2231fwg',
+      name: 'Brown Purse',
+      description: 'Product Description',
+      image: 'brown-purse.jpg',
+      price: 120,
+      category: 'Accessories',
+      quantity: 0,
+      inventoryStatus: 'OUTOFSTOCK',
+      rating: 4
+    },
+    {
+      id: '1006',
+      code: 'bib36pfvm',
+      name: 'Chakra Bracelet',
+      description: 'Product Description',
+      image: 'chakra-bracelet.jpg',
+      price: 32,
+      category: 'Accessories',
+      quantity: 5,
+      inventoryStatus: 'LOWSTOCK',
+      rating: 3
+    },
+    {
+      id: '1007',
+      code: 'mbvjkgip5',
+      name: 'Galaxy Earrings',
+      description: 'Product Description',
+      image: 'galaxy-earrings.jpg',
+      price: 34,
+      category: 'Accessories',
+      quantity: 23,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+    },
+    {
+      id: '1008',
+      code: 'vbb124btr',
+      name: 'Game Controller',
+      description: 'Product Description',
+      image: 'game-controller.jpg',
+      price: 99,
+      category: 'Electronics',
+      quantity: 2,
+      inventoryStatus: 'LOWSTOCK',
+      rating: 4
+    }]
+
   return (
     <div className={styles.masterContainer} >
       <Header title={"VIEW ALL ITEMS"} />
       <div className={styles.tableContainer} >
-        <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
+        <DataTable value={products} stripedRows tableStyle={{ minWidth: '50rem' }}>
+          <Column field="code" header="Code"></Column>
+          <Column field="name" header="Name"></Column>
+          <Column field="category" header="Category"></Column>
+          <Column field="quantity" header="Quantity"></Column>
+        </DataTable>
       </div>
     </div>
-  )
+  );
 }
 
-export default ViewItem
+export default ViewItem;
